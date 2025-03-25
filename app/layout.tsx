b39d977aa6b1bee709a3c187b0916ec4f6669977
Jacksonmills <jacksonemills@gmail.com>
2025-03-24 22:54:54 -0500

    feat: restructure PostHogProvider integration and update analytics configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,36 +34,37 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <PostHogProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <div className="flex flex-col items-center justify-center fixed top-4 right-4 z-50 bg-muted/50 rounded-full p-1">
-              <ModeToggle />
-              <Button
-                className="h-7 w-7"
-                size="icon"
-                variant="ghost"
-                asChild
-              >
-                <a href="https://github.com/Jacksonmills/jem-ui">
-                  <Icons.gitHub className="w-6 h-6" />
-                </a>
-              </Button>
-            </div>
-            <div className="isolate">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="flex flex-col items-center justify-center fixed top-4 right-4 z-50 bg-muted/50 rounded-full p-1">
+            <ModeToggle />
+            <Button
+              className="h-7 w-7"
+              size="icon"
+              variant="ghost"
+              asChild
+            >
+              <a href="https://github.com/Jacksonmills/jem-ui">
+                <Icons.gitHub className="w-6 h-6" />
+              </a>
+            </Button>
+          </div>
+          <div className="isolate">
+
+            <PostHogProvider>
               {children}
-              <footer className="text-muted-foreground text-sm p-4 border-t">
-                Built by <Button className="text-blue-500 p-0" variant="link"><a href="https://twitter.com/jacksonmills">Jacksonmills</a></Button>. The source code is available on <Button className="text-blue-500 p-0" variant="link"><a href="https://github.com/Jacksonmills/jem-ui">GitHub</a></Button>.
-              </footer>
-            </div>
-          </ThemeProvider>
-          <SpeedInsights />
-          <Analytics />
-        </PostHogProvider>
+            </PostHogProvider>
+            <footer className="text-muted-foreground text-sm p-4 border-t">
+              Built by <Button className="text-blue-500 p-0" variant="link"><a href="https://twitter.com/jacksonmills">Jacksonmills</a></Button>. The source code is available on <Button className="text-blue-500 p-0" variant="link"><a href="https://github.com/Jacksonmills/jem-ui">GitHub</a></Button>.
+            </footer>
+          </div>
+        </ThemeProvider>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
