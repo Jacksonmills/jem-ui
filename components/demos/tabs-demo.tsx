d1a126a37e335799450c445a5e8ddf7b73751907
Jacksonmills <jacksonemills@gmail.com>
2025-03-28 16:30:31 -0500

    refactor: adjust TabsDemo component width for responsive design

--- components/demos/tabs-demo.tsx
+++ components/demos/tabs-demo.tsx
@@ -13,7 +13,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export function TabsDemo() {
   return (
-    <Tabs defaultValue="account" className="w-[400px]">
+    <Tabs defaultValue="account" className="sm:w-[400px]">
       <TabsList className="grid w-full grid-cols-2">
         <TabsTrigger value="account">Account</TabsTrigger>
         <TabsTrigger value="password">Password</TabsTrigger>
